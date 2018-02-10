5ad2d87c253a4d42ed1d20efc167866de2dcd690
mcevoywj <35478956+mcevoywj@users.noreply.github.com>
2018-02-10 16:43:32 -0500

    Authorization complete

--- web-api-auth-examples-master/authorization_code/app.js
+++ web-api-auth-examples-master/authorization_code/app.js
@@ -12,9 +12,9 @@ var request = require('request'); // "Request" library
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '699b847baf514303a6ef8ea7433c7f27'; // Your client id
+var client_secret = 'b0c53709d38f4d7c8ef7c7fc5395cb52'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
